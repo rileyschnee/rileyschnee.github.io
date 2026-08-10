fd14d9a830034e5a435179a7dadc5200a7bfff67
Riley Schnee <rileyschnee@gmail.com>
2026-08-09 22:08:39 -0400

    First make it exist, then make it better - initial site structure using Astro

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+// src/constants.js
+export const SITE_METADATA = {
+  title: "Riley's Domain",
+  description: "Senior Software Engineer specializing in extensible frameworks and creative gameplay loops.",
+  domain: "https://rileyschnee.com"
+};
+
+export const SITE_NAV = {
+    pages: {
+        Home: "/",
+        About: "/about",
+        Games: "/games",
+    }
+}
+
+export const IDENTITY = {
+  firstName: "Riley",
+  lastName: "Schnee",
+  title: "Senior Software Engineer",
+  company: "Meta",
+  socialLinks: {
+    Github: "https://github.com/rileyschnee",
+    LinkedIn: "https://www.linkedin.com/in/riley-schnee/",
+    Itch: "https://rileyschnee.itch.io/"
+  }
+};
